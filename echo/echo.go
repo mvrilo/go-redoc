d276f64915dbba428271404ddd2a8c2d154b7d59
ttyS3 <ttys3.rust@gmail.com>
2023-08-09 00:49:04 +0800

    fix(echo): avoid duplicated body response

--- echo/echo.go
+++ echo/echo.go
@@ -11,6 +11,9 @@ func New(doc redoc.Redoc) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(ctx echo.Context) error {
 			handle(ctx.Response(), ctx.Request())
+			if ctx.Response().Committed {
+				return nil
+			}
 			return next(ctx)
 		}
 	}
